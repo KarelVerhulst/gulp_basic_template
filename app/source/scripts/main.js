3b7aa459fe0891c4377604f12736b850c808b30a
K_ar_el <karel.verhulst@student.howest.be>
2017-03-02 20:17:40 +0100

    Update main.js

--- app/source/scripts/main.js
+++ app/source/scripts/main.js
@@ -1,12 +1,1 @@
 
-
-init = function(){
-	var btn = document.getElementById('btnShow'),
-		textarea = document.getElementById('txtpackage');
-
-	btn.addEventListener('click', function(){
-		textarea.value = installGulpPackages();
-	});
-};
-
-init();
